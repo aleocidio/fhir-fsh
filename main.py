be10d1652cddca396acca99a679b53457a10628a
aleocidio <aleocidio@gmail.com>
2022-04-22 20:30:07 -0300

    scrapper funcional + extração json e tabulação

---
+++ main.py
@@ -0,0 +1,94 @@
+# Scrapper de arquivos do Simplifier
+
+# Importando dependências
+import requests
+import zipfile
+import io
+import os
+import json
+import pandas as pd
+from sqlalchemy import null
+
+# Variáveis globais
+
+url_download = 'https://simplifier.net/redenacionaldedadosemsaude/download?format=json'
+output_dir = 'simplifierRNDS/'
+current_dir = os.getcwd()
+
+
+# Dataframe para armazenar resultados
+recursos = pd.DataFrame()
+# Quando o pandas recebe uma lista de dicionários, cada dict é uma linha,
+# as chaves são as colunas e os valores são os valores das células
+
+recursos_list = []
+
+def download(url):
+    """Baixa o arquivo do simplifier contendo os json de recursos e extrai o zip"""
+
+    print("Iniciando download")
+    req = requests.get(url_download)
+
+
+    with io.open('simplifierRNDS.zip','wb') as output_file:
+        output_file.write(req.content)
+    print('Download concluído')
+    
+    try:
+        with zipfile.ZipFile("simplifierRNDS.zip", mode="r") as archive:
+            for info in archive.infolist():
+                print("Extraindo ", info.filename)
+                archive.extract(info.filename, path=output_dir)
+    except zipfile.BadZipFile as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    print('#### START #####')
+    
+    download(url_download)
+    
+    arquivos_json = os.listdir(os.path.join(current_dir, output_dir))
+
+    for arquivo in arquivos_json:
+        file_dir = os.path.join(current_dir, output_dir, arquivo)
+
+        try:
+            f = io.open(file_dir, encoding="utf-8")
+            print("Abrindo arquivo", file_dir)
+
+            data = json.load(f)
+            
+            keys = ['url','status','version','publisher','id','resourceType','name']
+            dict = {}
+            for key in keys:
+                try:
+                    dict[key] = data[key]
+                    if key == 'version':
+                        print(data[key])
+                except KeyError:
+                    dict[key] = ''
+
+                try:
+                    dict['lastUpdated'] = data['meta']['lastUpdated']
+                except KeyError:
+                    dict['meta'] = ''
+                
+            recursos_list.append(dict)
+            f.close()
+
+        except UnicodeDecodeError as error:
+            f.close()
+            print(error)
+    
+    recursos = pd.DataFrame(recursos_list)
+    print(recursos_list)
+    recursos.to_excel('recursos.xlsx')
+
+    print('#### END ####')
+
+# data quality
+# recursos sem id
+# recursos sem versao
+# analisar o cross reference
+
